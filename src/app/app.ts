adf1eb790639de6710348679c51f3ba7e0cbdc94
gordeusbraga <mb326780@gmail.com>
2025-10-21 20:49:19 -0300

    Feat: Create login page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Login } from './login/login';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
