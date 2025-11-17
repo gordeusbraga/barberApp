98133d868756c4336d0d708091c77b46aba04d2b
gordeusbraga <mb326780@gmail.com>
2025-11-17 15:08:24 -0300

    Fix: Modificacao do env para publicacao no vercel

--- src/app/services/profileService.ts
+++ src/app/services/profileService.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { environment } from '../../environments/environment';
+import { environment } from 'environments/environment';
 import { AuthService } from './authService';
 import { switchMap, map, filter, shareReplay } from 'rxjs/operators';
 
