828c7d274710529687c330f2e772e6b1a5e66c03
gordeusbraga <mb326780@gmail.com>
2025-11-17 16:06:51 -0300

    Fix: Redefinindo build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@
 
 export const environment = {
     production: true,
-    supabaseUrl: process.env['SUPABASE_URL']!,
-    supabaseKey: process.env['SUPABASE_KEY']!
+    supabaseUrl: 'https://pdufnwsgghtkvpvyahbv.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkdWZud3NnZ2h0a3ZwdnlhaGJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzMjU3NjMsImV4cCI6MjA3ODkwMTc2M30.nYs2O3k4kZo74oCI7KmN8tqZmoxARO8ty8vohz7jRsM'
 };
