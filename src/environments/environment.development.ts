e8147a23213015b821388b9b1f460c4621a59fe3
gordeusbraga <mb326780@gmail.com>
2025-11-17 16:04:59 -0300

    Fix: Add anon key

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+
+    supabaseUrl: 'https://pdufnwsgghtkvpvyahbv.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkdWZud3NnZ2h0a3ZwdnlhaGJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzMjU3NjMsImV4cCI6MjA3ODkwMTc2M30.nYs2O3k4kZo74oCI7KmN8tqZmoxARO8ty8vohz7jRsM'
+};
